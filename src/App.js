37d65d33ec99b7eab51a96b964084cd2eb36abf6
SatvikIntellipaat <satvikintellipaat@gmail.com>
2025-03-24 21:56:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import WeatherApp from './WeatherApp/weatherApp';
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp />
+    </div>
+  );
+}
+
+export default App;
